ac590e1222f40f98e524e3e973a3e537d728dd65
Satwik Bhardwaj <satwikbhardwaj123@gmail.com>
2022-06-20 16:29:24 +0530

    Project of 3d box is added

---
+++ functionality.js
@@ -0,0 +1,7 @@
+var anyChange = document.querySelector('Chng_btns');
+
+document.getElementById("btns").addEventListener("mouseover", makeChange);
+
+function makeChange(){
+    document.body.style.backgroundColor="#333333";
+}
